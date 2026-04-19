0e57121dcca2f8c947b6998d5e8093b9f080c6ec
Juan Wiley <224348085+juanwiley@users.noreply.github.com>
2026-04-19 00:39:48 -0400

    Add white cat sprite, wire up spritesheet, flip direction on move

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 const SPEED = 80
 
+@onready var sprite = $Sprite2D
+
 func _physics_process(delta):
 	var direction = Vector2.ZERO
 
@@ -9,6 +11,9 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):  direction.x -= 1
 	if Input.is_action_pressed("ui_down"):  direction.y += 1
 	if Input.is_action_pressed("ui_up"):    direction.y -= 1
+
+	if direction.x != 0:
+		sprite.flip_h = direction.x < 0
 
 	velocity = direction.normalized() * SPEED
 	move_and_slide()
